63ed4d461766933e14d7f032e08294698b420d13
root <root@localhost.localdomain>
2018-04-14 16:22:34 -0500

    build upload form with flask-wtf

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import TextField,SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileRequired,FileField,FileAllowed
+
+
+
+class UploadForm(FlaskForm):
+    photo = FileField("Photo", validators=[FileRequired(),
+            FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+
+    description = TextField('Description', validators=[DataRequired()])
 
